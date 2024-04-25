58fe5854ea0fa785e771bf60f17cacadc7359d83
IngridGdesigns <garciai@protonmail.com>
2024-04-25 11:41:28 -0700

    added home route

---
+++ backend/routes/home.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+// Import your PostgreSQL connection pool
+
+let api = express.Router();
+
+// Read all library staff members
+api.get('/', async (req, res) => {
+    console.log('welcome home');
+});
