1fca2757f2e78af0405215140fd990c3dac3019c
IngridGdesigns <garciai@protonmail.com>
2024-04-30 11:50:05 -0700

    clean up

--- backend/models/authors.js
+++ backend/models/authors.js
@@ -5,11 +5,11 @@ const pool = require('../database')// Import your PostgreSQL connection pool
 ********************************/
 
 // Gets all authors table 
-const getAuthors = (request, response) => {
+const getAuthors = async (req, response) => {
    
-    const client = pool.connect();
+    const client = await pool.connect();
     
-    client.query('SELECT * FROM author', (err, results) => {
+    await client.query('SELECT * FROM author', (err, results) => {
         // handleErrorOrReturnData();
         if (err) {
             console.log('error oh noes!!', err)
@@ -25,29 +25,29 @@ const getAuthors = (request, response) => {
 }
 
 // get author by id
-const getAuthorById = (req, res) => {
-    const client = pool.connect();
-    let id = parseInt(req.params.author_id);
+const getAuthorById = async (req, response) => {
+    const client = await pool.connect();
+    const authorId = parseInt(req.params.author_id);
 
-    client.query('SELECT * FROM author WHERE author_id = $1', [id], (err, result) => {
+    client.query('SELECT * FROM author WHERE author_id = $1', [authorId], (err, results) => {
       if (err) {
           response.status(500).send(err);
           client.release()
       } 
       else { //response.json(dbitems.rows[0] )
-          response.status(200).json(result.rows[0])
+          response.status(200).json(results.rows[0])
           client.release()
       }
     })
 }
 
 // add new author
-const createAuthor = (req, response) => {
-    const client = pool.connect();
+const createAuthor = async (req, response) => {
+    const client = await pool.connect();
 
     let author_name = req.body.author_name;
 
-    client.query('INSERT INTO author(author_name) VALUES($1) RETURNING *',
+    await client.query('INSERT INTO author(author_name) VALUES($1) RETURNING *',
         [author_name], (err, result) => {
             if (err) {
                 response.status(500).send('Server error')
@@ -60,30 +60,29 @@ const createAuthor = (req, response) => {
 }
 
 // update author by author id
-const updateAuthor= (req, res) => {
-    const client = pool.connect();
+const updateAuthor = async (req, response) => {
+    const client = await pool.connect();
     
     const authorId = parseInt(req.params.author_id);
     const { author_name } = req.body;
     
-    let res = client.query(
-        'UPDATE author SET author_name = $1 WHERE author_id = $5 RETURNING *', [author_name, authorId], (res, res) => {
+    await client.query(
+        'UPDATE author SET author_name = $1 WHERE author_id = $5 RETURNING *', [author_name, authorId], (res, result) => {
             if (error) {
                 throw error;
             }
-            res.status(200).json(`This author with the id: ${authorId} was updated!`)
+            response.status(200).json(`This author with the id: ${authorId} was updated!`)
     })  
 }
 
 // delete by author id
-const deleteAuthor = (req, res) => {
-    const client = pool.connect();
+const deleteAuthor = async (req, response) => {
+    const client = await pool.connect();
     const authorId = parseInt(req.params.author_id);
     
     const { author_name } = req.body;
 
-    client.query(
-            'DELETE FROM author WHERE author_id = $1', [authorId], (err, results) => {
+    await client.query('DELETE FROM author WHERE author_id = $1', [authorId], (err, res) => {
          if(err){
            console.log('Oh noes you have an error!!')
            response.status(500).send('There is a server error')
@@ -98,6 +97,7 @@ const deleteAuthor = (req, res) => {
 }
 
 
+
 module.exports = {
         getAuthors,
         getAuthorById,
@@ -105,3 +105,4 @@ module.exports = {
         updateAuthor,
         deleteAuthor,
 }
+
